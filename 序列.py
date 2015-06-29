913eecb800552a51293ada2ad5489e96b0b2c797
mokast <465119455@qq.com>
2015-06-29 09:19:33 +0800

    ok

---
+++ 序列.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+#序列包含6钟（列表、元组、字符串、unicode字符串、buffer对象、xrange对象）
+#序列、映射、集合都属于容器
+greeting="Hello World"
+print "greeting:"+greeting
+print "greeting[0]:"+greeting[0]
+print "greeting[:3]:"+greeting[:3]
+print "greeting[-1]:"+greeting[-1]
+print "greeting[2:5]:"+greeting[2:5]
+print "greeting[-4:-1]:"+greeting[-4:-1]
+print "greeting[-3:]:"+greeting[-3:]
+print "greeting[::2]:"+greeting[::2]
+print "greeting[::-1]:"+greeting[::-1]
+print "greeting*2:"+greeting*2
